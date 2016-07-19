ff6d2a8a4d64c32aef48afffb03759987f593009
Aluno <aluno@\E.elaborata.lan>
2016-07-18 21:54:49 -0300

    adicionado validações

--- pedidos.js
+++ pedidos.js
@@ -1,20 +1,54 @@
 $(document).ready(function(){
   
+ 
+	$('.quant').keydown(function(event) {
+  
+	  
+	  if( ( event.keyCode>=48 && event.keyCode <=59) || ( event.keyCode>=96 && event.keyCode <=105) || ( event.keyCode>=35 && event.keyCode <=40) || event.keyCode == 20){
+	      return true;
+	  }else{
+	    return false;
+	  }
+	  
+	});
+  
+	$('.val-unit').keydown(function(event) {
+  
+	 
+	  if( ( event.keyCode>=48 && event.keyCode <=59) || ( event.keyCode>=96 && event.keyCode <=105) || ( event.keyCode>=35 && event.keyCode <=40)){ 
+	    
+	    var val = $('.val-unit').val();
+	    var digitado = String.fromCharCode(event.keyCode);
+	      return true;
+	  }else{
+	    return false;
+	  }
+	  
+	});
   
 	var totalGeral = 0;
 	  $("#add").click(function(){
+	     
+	    var formValido = true;
+	    $('#form div.erro').removeClass('erro');
 	    
+	    $('#form input').each(function(index,elem) {	     
+	     
+		if($(elem).val() == ''){
+		  $(elem).parent().addClass('erro');
+		  formValido = false;
+		}		
 	      
+	    });
+	    
+	    if(formValido == false) return;
+	    
 	    var ind = $('#lista tbody tr').length + 1;
 	    var cod = $("[name='produto-cod']").val();
 	    var produto = $("[name='produto']").val();
 	    var qtde = $("[name='qtde']").val();
 	    var unit = $("[name='val-unit']").val();
-	
-	    if( cod == '') return;
-	    if( produto == '') return;
-	    if( qtde == '') return;
-	    if( unit == '') return;
+
 			  
 	    var total = parseFloat(unit) * parseFloat(qtde);
 	    
